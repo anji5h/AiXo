ae4ab0304b458b106e112f57e7d631ecf58ea4ab
anjish <40964308+anji5h@users.noreply.github.com>
2025-07-17 22:41:04 +0300

    updated js frontend to ts, setup tailwindcss

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <div className="w-full h-8 border-2 border-amber-100">
+
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,5 @@
+export const content = ["./index.html", "./src/**/*.{js,ts,jsx,tsx,vue}"];
+export const theme = {
+    extend: {},
+};
+export const plugins = [];
